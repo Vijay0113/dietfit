c9fbca8dcec0f777ad0dc3c63eb483e9d99a0a6a
VIJAY R <145474381+Vijay0113@users.noreply.github.com>
2023-11-27 14:14:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,35 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const { Sequelize, DataTypes } = require('sequelize');
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-// SQLite setup
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: path.join(__dirname, 'database.sqlite'), // Change the database file path
-});
-
-// Define the User model
-const User = sequelize.define('User', {
-  first_name: DataTypes.STRING,
-  last_name: DataTypes.STRING,
-  email: DataTypes.STRING,
-  phone: DataTypes.STRING,
-  password: DataTypes.STRING,
-});
-
-// Sync the model with the database
-sequelize.sync();
-
 app.set('port', process.env.PORT || 5000);
-
-// Routes
-
-// Home page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
